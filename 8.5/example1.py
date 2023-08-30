882d594e6fe6fc8242257d291bb9ff4513d6a6f3
awesomednd <dagan.drennan@gmail.com>
2023-08-30 13:39:21 +0200

    added comments to 8.5 example1.py and example2.py

--- 8.5/example1.py
+++ 8.5/example1.py
@@ -1,3 +1,3 @@
-num1 = 10
-if num1 == 10:
-    print("True")
+num1 = 10 # Setting the value of num1 to 10
+if num1 == 10: # Checking if the value of num1 is equal to 10
+    print("True") # If it is we print that it is true
--- 8.5/example2.py
+++ 8.5/example2.py
@@ -1,4 +1,4 @@
-valueInput = int(input("Enter a number"))
-storedValue = 100
-if valueInput > storedValue:
-    print("It is more than 100")
+valueInput = int(input("Enter a number")) # Setting valueInput to the interger of the inputted value
+storedValue = 100 # Setting the value of storedValue to 100
+if valueInput > storedValue: # Checking if the value of valueInput is greater than storedValue
+    print("It is more than 100") # If it is print It is more than 100
