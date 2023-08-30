a229bb1b303991f8cb8bee58bf99f833b690f785
awesomednd <dagan.drennan@gmail.com>
2023-08-30 13:35:20 +0200

    added comments for 8.4

--- 8.4/example1.py
+++ 8.4/example1.py
@@ -1,3 +1,3 @@
-colour = input("Enter a colour")
-name = input("Enter a name ")
-print(name, " your favourite color is ", colour)
+colour = input("Enter a colour") # Setting the value of colour to the value inputed
+name = input("Enter a name ") # Settin the value name to the value inputed
+print(name, " your favourite color is ", colour) # printing the value name and then you favourit colour is and then the value of colour
--- 8.4/example2.py
+++ 8.4/example2.py
@@ -1,7 +1,7 @@
-x = 1
-y = 2
-z = 3
-total = x + y + z
-value1 = int(input("Enter the first value"))
-print(total)
-print(value1)
+x = 1 # Setting the value x to 1
+y = 2 # Setting the value y to 2
+z = 3 # Setting the value z to 3
+total = x + y + z # Setting the value of total to the total of x, y and z
+value1 = int(input("Enter the first value")) # Setting the value of value1 to the interger of the value inputed
+print(total) # Printing the value of the variable total
+print(value1) # Printing the value of the variable value1
