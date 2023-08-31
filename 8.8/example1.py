3deafe5a95871b9afe810cdb544a17642b0eefa0
awesomednd <dagan.drennan@gmail.com>
2023-08-31 14:55:58 +0200

    Added coments to 8.8

--- 8.8/example1.py
+++ 8.8/example1.py
@@ -1,6 +1,6 @@
-count = 0
-continueInput = "Yes"
-while continueInput == "Yes":
-    continueInput = input("Do you want to continue")
-    count = count + 1
-print("You continued ", str(count - 1), " times")
+count = 0 # Swtting the value of count to 0
+continueInput = "Yes" # Setting the value of continueInput to Yes
+while continueInput == "Yes": # Running a loop while continueInput is Yes
+    continueInput = input("Do you want to continue") # Setting the value of continueInput to the value inputed
+    count = count + 1 # Adding 1 to the value of count
+print("You continued ", str(count - 1), " times") # Printing
--- 8.8/example2.py
+++ 8.8/example2.py
@@ -1,12 +1,12 @@
-from random import randint
-arrayData = []
+from random import randint # Importing randint from random
+arrayData = [] # Setting the value of array data to []
 
-for y in range(1, 101):
-    arrayData += [randint(0, 25565)]
+for y in range(1, 101): # Runnig a loop 100 times
+    arrayData += [randint(0, 25565)] # Adding a random number to the value of arrayData
 
-count = 0 
-for x in range(0, 100):
-    if arrayData[x] > 50:
-        count = count + 1
+count = 0 # Setting the value of count to 0
+for x in range(0, 100): # Running a loop 100 times
+    if arrayData[x] > 50: # Checking if the value in arrayData at the current loop count is grater than 50
+        count = count + 1 # Adding 1 to count
 
-print("count is ", count)
+print("count is ", count) # Printing
