5be908504e3cb3f690a9625b95145f6024331b54
awesomednd <dagan.drennan@gmail.com>
2023-08-31 14:42:13 +0200

    Finished comments for 8.5

--- 8.5/example8.py
+++ 8.5/example8.py
@@ -1,8 +1,8 @@
-mark1 = int(input("Enter the mark for test 1"))
-mark2 = int(input("Enter the mark for test 2"))
-if mark1 >= 90 and mark2 >= 90:
-    print("Well done you got top markes on both tests")
-elif mark1 >= 90 or mark2 >= 90:
-    print("Well done you got top markes on one of the tests")
-else:
-    print("You didn't quite get top markes on the tests, try again next time")
+mark1 = int(input("Enter the mark for test 1")) # Setting the value of mark1 to the Interger of the value inputed
+mark2 = int(input("Enter the mark for test 2")) # Setting the value of mark2 to the Interger of the value inputed
+if mark1 >= 90 and mark2 >= 90: # Checking if mark1 and mark2 are both 90 or higher
+    print("Well done you got top markes on both tests") # Printing 
+elif mark1 >= 90 or mark2 >= 90: # Checking if mark1 or mark2 is 90 or higher
+    print("Well done you got top markes on one of the tests") # Printing
+else: # Else 
+    print("You didn't quite get top markes on the tests, try again next time") # Printing
--- 8.5/example9.py
+++ 8.5/example9.py
@@ -1,9 +1,9 @@
-number1 = int(input("Enter a number"))
-number2 = int(input("Enter a number"))
-number3 = int(input("Enter a number"))
-if number1 > number2 and number1 > number3:
-    print(number1)
-elif number2 > number3:
-    print(number2)
-else:
-    print(number3)
+number1 = int(input("Enter a number")) # Setting the value of number1 to the interger of the value inputed
+number2 = int(input("Enter a number")) # Setting the value of number2 to the interger of the value inputed
+number3 = int(input("Enter a number")) # Setting the value of number3 to the interger of the value inputed
+if number1 > number2 and number1 > number3: # Checking if number1 is greater than number2 and if number1 is greater than number 3
+    print(number1) # Printing number1
+elif number2 > number3: #Checking if number2 is greater than number3
+    print(number2) # Printing number2
+else: # Else
+    print(number3) # Printing number3
